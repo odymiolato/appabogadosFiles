e1343cfd592f060dd40a2113b916cc9966284b11
Ody <125280969+odymiolato@users.noreply.github.com>
2024-07-16 10:33:41 -0400

    se modifico la manera de recibir la URL

--- saveFile.php
+++ saveFile.php
@@ -5,13 +5,13 @@ header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $target_dir = "../abogadosfiles/exp01/";
-    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    $target_dir = $_POST['URL'];;
+    $target_file = ".." . $target_dir . basename($_FILES["file"]["name"]);
 
     if (file_exists($target_file)) {
         echo json_encode([
             'status' => 1,
-            'message' => 'El archivo ya Existe.'
+            'message' => 'The file exists.'
         ]);
         return;
     }
@@ -19,13 +19,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
         echo json_encode([
             'status' => 2,
-            'message' => 'Archivo Subido.',
+            'message' => 'File Uploaded.',
             'filename' => basename($_FILES["file"]["name"])
         ]);
     } else {
         echo json_encode([
             'status' => 3,
-            'message' => 'Error al subir el archivo.',
+            'message' => 'Error uploading the file.',
         ]);
     }
 } else {
