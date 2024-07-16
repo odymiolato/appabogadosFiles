2f14b66ec859ef2230d8efffa2f0b4029d58cbfb
Ody <125280969+odymiolato@users.noreply.github.com>
2024-07-16 14:56:13 -0400

    cambios

--- src/views/componentes a revisar/clientes/GestionCliente.vue
+++ src/views/componentes a revisar/clientes/GestionCliente.vue
@@ -25,7 +25,7 @@ const currentEndpoint = ref('')
 
 const nombre = ref('')
 
-function openModal(type:any) {
+function openModal(type: any) {
   if (type === 'users') {
     modalTitle.value = 'Usuarios'
     currentEndpoint.value = 'https://tu-api.com/users'
@@ -41,7 +41,7 @@ function closeModal() {
   isModalOpen.value = false
 }
 
-function handleSelect(item:any) {
+function handleSelect(item: any) {
   nombre.value = item.name
   // Llenar otros campos si es necesario
 }
@@ -73,20 +73,92 @@ const paginationtabledata = [
 </script>
 
 <template>
-  <div>
-    <h3 class="text-3xl font-medium text-gray-700">
-      Gestion de Clientes
-    </h3>
 
+
+
+  <!-- <div>
+   
     <Buttons -buttonstext="Añadir Cliente" />
     <Buttons -buttonstext="Modificar" />
 
-    <!-- esto lo debo poner en un modal -->
+    -- esto lo debo poner en un modal --
     <Inputs typeinput="text" labeltext="nombre" />
     <Inputs typeinput="text" labeltext="contacto" />
     <Buttons -buttonstext="Guardar" />
     <Buttons -buttonstext="Cancelar" />
 
     <PaginationTable default-image="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" :columns="paginationtablecolumns" :tabledata="paginationtabledata" />
+  </div> -->
+  <h3 class="text-3xl font-medium text-gray-700">
+    Gestion de Clientes
+  </h3>
+
+  <div class="mt-4">
+    <div class="p-6 bg-white rounded-md shadow-md">
+      <form>
+        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div>
+            <label class="text-gray-700" for="nombre_cli">Nombre</label>
+            <input id="nombre_cli" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="apellido_cli">Apellido</label>
+            <input id="apellido_cli" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="fecnac_cli">Fecha de Nacimiento</label>
+            <input id="fecnac_cli" type="date"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="numdoc_cli">Número de Documento</label>
+            <input id="numdoc_cli" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="email_cli">Email</label>
+            <input id="email_cli" type="email"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="telefono_cli">Teléfono</label>
+            <input id="telefono_cli" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="fecini_cli">Fecha de Inicio</label>
+            <input id="fecini_cli" type="date"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="usercrea">Usuario Creador</label>
+            <input id="usercrea" type="number"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="usermod">Usuario Modificador</label>
+            <input id="usermod" type="number"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+        </div>
+
+        <div class="flex justify-end mt-4">
+          <button
+            class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+            Guardar
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
--- src/views/componentes a revisar/expediente/Expedientes.vue
+++ src/views/componentes a revisar/expediente/Expedientes.vue
@@ -25,7 +25,74 @@ const tableData = ref([
 </script>
 
 <template>
-  <div>
+  <h3 class="text-3xl font-medium text-gray-700">
+    Registrar Expedinte
+  </h3>
+
+  <div class="mt-4">
+    <div class="p-6 bg-white rounded-md shadow-md">
+      <form>
+        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div>
+            <label class="text-gray-700" for="codexp_exp">Código Expediente</label>
+            <input id="codexp_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="codabo_exp">Código Abogado</label>
+            <input id="codabo_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="motivo_exp">Motivo</label>
+            <input id="motivo_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="codcli_exp">Código Cliente</label>
+            <input id="codcli_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="fecini_exp">Fecha de Inicio</label>
+            <input id="fecini_exp" type="date"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="estatu_exp">Estatus</label>
+            <input id="estatu_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="tipexp_exp">Tipo de Expediente</label>
+            <input id="tipexp_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+
+          <div>
+            <label class="text-gray-700" for="codcon_exp">Código de Contraparte</label>
+            <input id="codcon_exp" type="text"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+          </div>
+        </div>
+
+        <div class="flex justify-end mt-4">
+          <button
+            class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+            Guardar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <!-- <div>
     <h3 class="text-3xl font-medium text-gray-700">
       Expedientes
     </h3>
@@ -36,7 +103,7 @@ const tableData = ref([
     <Inputs typeinput="text" labeltext="nombre" />
 
     <Dropdown labeltext="dropdown 1" :event="events" labal-item-noselected="Tipo" />
-    <!-- <Dropdown2 labeltext="dropdown 2" :items="events" labal-item-noselected="Estado" /> -->
+    <Dropdown2 labeltext="dropdown 2" :items="events" labal-item-noselected="Estado" />
     <div class="flex flex-col gap-4" label="estado">
       <Inputs typeinput="Radio" labeltext="Pendiente" />
       <Inputs typeinput="Radio" labeltext="Proceso" />
@@ -47,5 +114,5 @@ const tableData = ref([
     <Buttons -buttonstext="Cancelar" />
 
     <SimpleTable :tabledata="tableData" :columns="columns" />
-  </div>
+  </div> -->
 </template>
