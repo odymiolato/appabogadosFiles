a250aa496b2287ce6fa96c92dd541e2b7317f8ad
Ody <125280969+odymiolato@users.noreply.github.com>
2024-07-19 17:01:13 -0400

    cambios

--- src/views/abogados/GestionAbogados.vue
+++ src/views/abogados/GestionAbogados.vue
@@ -2,9 +2,10 @@
 import { computed, ref, onMounted } from 'vue';
 import Modal from '../../components/Modal.vue';
 import Inputs from '../../components/Inputs.vue';
-import { especialidades } from '../../class/all.class';
+import { abogados, especialidades } from '../../class/all.class';
 
 const showModal = ref(false);
+const Abogado = ref<abogados>(new abogados());
 /* @ts-ignore */
 const URL: string = import.meta.env.VITE_PATH_API;
 
@@ -66,6 +67,10 @@ function setEspecialidad(obj: especialidades) {
   handleAccept();
 }
 
+async function saveAbogado(){
+  console.info(Abogado.value);
+}
+
 onMounted(() => {
   GetEspecialidades();
 });
@@ -82,32 +87,36 @@ onMounted(() => {
           <div>
             <label class="text-gray-700" for="nombre_abo">Nombre</label>
             <input id="nombre_abo" type="text"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.nombre_abo">
           </div>
 
           <div>
             <label class="text-gray-700" for="direcc_abo">Dirección</label>
             <input id="direcc_abo" type="text"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.direcc_abo">
           </div>
 
           <div>
             <label class="text-gray-700" for="telefo_abo">Teléfono</label>
             <input id="telefo_abo" type="text"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.telefo_abo">
           </div>
 
           <div>
             <label class="text-gray-700" for="celula_abo">Cédula</label>
             <input id="celula_abo" type="text"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.celula_abo">
           </div>
 
           <div class="">
             <label class="text-gray-700" for="tipo_espcialidad_abo">Especialidad</label>
             <div class="flex gap-2 justify-center items-center">
-              <input id="fecnac_abo" type="text"
-                class="w-[20%] mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              <input id="fecnac_abo" type="text" disabled
+                class="w-[20%] mt-2 border-gray-200  rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
                 v-model="especialidadSelected.tipesp_tip" readonly>
 
               <input id="fecnac_abo" type="text"
@@ -129,25 +138,28 @@ onMounted(() => {
           <div>
             <label class="text-gray-700" for="fecnac_abo">Fecha de Nacimiento</label>
             <input id="fecnac_abo" type="date"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.fecnac_abo">
           </div>
 
           <div>
             <label class="text-gray-700" for="email_abo">Correo Electrónico</label>
             <input id="email_abo" type="email"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.email_abo">
           </div>
 
           <div>
             <label class="text-gray-700" for="fecini_abo">Fecha de Inicio</label>
             <input id="fecini_abo" type="date"
-              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500">
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="Abogado.fecini_abo">
           </div>
 
         </div>
 
         <div class="flex justify-end mt-4">
-          <button
+          <button type="button" @click="saveAbogado()"
             class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
             Guardar
           </button>
