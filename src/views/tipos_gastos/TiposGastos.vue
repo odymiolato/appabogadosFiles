db2b027a38665be1f507ce607df095bc458a6f49
Ody <125280969+odymiolato@users.noreply.github.com>
2024-07-29 11:28:33 -0400

    cambios

--- src/views/tipos_gastos/TiposGastos.vue
+++ src/views/tipos_gastos/TiposGastos.vue
@@ -1,75 +1,54 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import Inputs from '../../../components/Inputs.vue';
-import PaginationTable from '../../../components/tablas/PaginationTable.vue'
-import Buttons from '../../../components/Buttons.vue'
+import { tipos_gastos } from '../../class/all.class';
+import { addAlert } from '../../stores/alerts';
 
-const events = ref([
-  { index: 1, item: 'ejemplo' },
-  { index: 2, item: 'ejemplo 2' },
-])
-const columns = ref([
-  { key: 'city', label: 'City' },
-  { key: 'totalOrders', label: 'Total orders' },
-])
+const tipo = ref<tipos_gastos>(new tipos_gastos());
+/* @ts-ignore */
+const URL: string = import.meta.env.VITE_PATH_API;
 
-const tableData = ref([
-  { city: 'New York', totalOrders: 150 },
-  { city: 'Los Angeles', totalOrders: 200 },
-  // Agrega más datos aquí
-])
+function clearPage(): void {
+  tipo.value = new tipos_gastos();
+}
 
-const isModalOpen = ref(false)
-const modalTitle = ref('')
-const currentEndpoint = ref('')
-
-const nombre = ref('')
-
-function openModal(type: any) {
-  if (type === 'users') {
-    modalTitle.value = 'Usuarios'
-    currentEndpoint.value = 'https://tu-api.com/users'
+function validar(): boolean {
+  if (tipo.value.descri_tga === '' || tipo.value.tipgas_tga === '') {
+    addAlert(1, "Debe de llenar todos los campos.");
+    return false;
   }
-  else if (type === 'products') {
-    modalTitle.value = 'Productos'
-    currentEndpoint.value = 'https://tu-api.com/products'
+  return true;
+}
+
+async function saveTipoGasto(): Promise<void> {
+  try {
+    if (!validar()) {
+      return;
+    }
+
+    const response = await fetch(URL + 'tiposgastos', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(tipo.value)
+
+    });
+
+    if (response.ok) {
+      if (await response.json()) {
+        addAlert(2, "Tipo de Gasta resgistrado satisfactoriamente.")
+        clearPage()
+      }
+    } else {
+      addAlert(3, "Problemas con la peticion.")
+    }
+
+  } catch (error) {
+    console.log(error);
+    addAlert(3, "Problemas al reaizar la peticion.");
   }
-  isModalOpen.value = true
 }
 
-function closeModal() {
-  isModalOpen.value = false
-}
-
-function handleSelect(item: any) {
-  nombre.value = item.name
-  // Llenar otros campos si es necesario
-}
-
-const paginationtablecolumns = [
-  {
-    name: 'Nombre',
-    field: 'nombre',
-    hasImage: true,
-  },
-  {
-    name: 'Contacto del Cliente',
-    field: 'contacto',
-
-  },
-
-]
-
-const paginationtabledata = [
-  {
-    nombre: {
-      text: 'Maria',
-      image: '',
-    },
-    contacto: '2345789234576',
-
-  },
-]
 </script>
 
 <template>
@@ -78,48 +57,32 @@ const paginationtabledata = [
   </h3>
 
   <div class="mt-4">
-        <div class="p-6 bg-white rounded-md shadow-md">
-            <h2 class="text-lg font-semibold text-gray-700 capitalize">Configuración de Tipos de Gastos</h2>
+    <div class="p-6 bg-white rounded-md shadow-md">
+      <!-- <h2 class="text-lg font-semibold text-gray-700 capitalize">Configuración de Tipos de Gastos</h2> -->
 
-            <form>
-                <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-                    <div>
-                        <label class="text-gray-700" for="tipgas_tga">Tipo de Gasto</label>
-                        <input id="tipgas_tga" type="text" maxlength="1" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-
-                    <div>
-                        <label class="text-gray-700" for="descri_tga">Descripción</label>
-                        <input id="descri_tga" type="text" maxlength="50" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-
-                    <div>
-                        <label class="text-gray-700" for="usercrea">Usuario Creador</label>
-                        <input id="usercrea" type="text" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-
-                    <div>
-                        <label class="text-gray-700" for="usermod">Usuario Modificador</label>
-                        <input id="usermod" type="text" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-
-                    <div>
-                        <label class="text-gray-700" for="fechcrea">Fecha de Creación</label>
-                        <input id="fechcrea" type="date" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-
-                    <div>
-                        <label class="text-gray-700" for="fechmod">Fecha de Modificación</label>
-                        <input id="fechmod" type="date" class="w-full mt-2 border-gray-200 rounded-md focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500">
-                    </div>
-                </div>
-
-                <div class="flex justify-end mt-4">
-                    <button class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
-                        Guardar
-                    </button>
-                </div>
-            </form>
+      <div>
+        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+          <div>
+            <label class="text-gray-700" for="descri_tga">Codigo</label>
+            <input id="descri_tga" type="text" maxlength="1" 
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500 uppercase"
+              v-model="tipo.tipgas_tga">
+          </div>
+          <div>
+            <label class="text-gray-700" for="descri_tga">Nombre</label>
+            <input id="descri_tga" type="text" maxlength="50"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+              v-model="tipo.descri_tga">
+          </div>
         </div>
+
+        <div class="flex justify-end mt-4">
+          <button type="button" @click="saveTipoGasto()"
+            class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700 ">
+            Guardar
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
