0a68c2f43b98a97f630e5f645f6ce02e736b9a04
Ody <125280969+odymiolato@users.noreply.github.com>
2024-08-07 15:24:17 -0400

    cambios

---
+++ createFolder.php
@@ -0,0 +1,47 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Invalid request method'
+    ]);
+    exit;
+}
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data['USERID'])) {
+    echo json_encode([
+        'status' => 3,
+        'message' => 'Sin id de usuario.'
+    ]);
+    exit;
+}
+
+$folderName = preg_replace('/[^a-zA-Z0-9_-]/', '', $data['USERID']);
+// $directoryPath = __DIR__ . '/' . $folderName;
+$directoryPath = '../abogadosfiles/exp' . $folderName;
+
+if (file_exists($directoryPath)) {
+    echo json_encode([
+        'status' => 3,
+        'message' => 'La carpeta ya existe.'
+    ]);
+    exit;
+}
+
+if (mkdir($directoryPath, 0777, true)) {
+    echo json_encode([
+        'status' => 2,
+        'message' => 'Carpeta creada correctamente.'
+    ]);
+} else {
+    echo json_encode([
+        'status' => 3,
+        'message' => 'Error al crear carpeta.'
+    ]);
+}
