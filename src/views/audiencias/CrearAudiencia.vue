14982f3f66b4e0339bc1a4c6f848396c9b84a3a7
Gerson Gabriel Vargas Batista <117436027+Nexopower@users.noreply.github.com>
2024-08-12 12:06:53 -0400

    el diseno de audiencia

--- src/views/audiencias/CrearAudiencia.vue
+++ src/views/audiencias/CrearAudiencia.vue
@@ -3,9 +3,9 @@ import { computed, onMounted, ref } from 'vue'
 import { useTableData } from '../../composables/useTableData'
 import { addAlert } from '../../stores/alerts'
 import type { abogados, clientes, contrapartes, tipos_expedientes, tribunales } from '../../class/all.class'
-import { ExpedienteDetalle, expedientes } from '../../class/all.class'
+import { ExpedienteDetalle, audiencias } from '../../class/all.class'
 
-const ExpedienteHeader = ref<expedientes>(new expedientes())
+const AudienciaHeader = ref<audiencias>(new audiencias())
 const tap = ref<number>(1)
 const showModal = ref<boolean>(false)
 const TipoExpedientes = ref<tipos_expedientes[]>([])
@@ -17,10 +17,13 @@ const AbogadosList = ref<{ id: number; name: string }[]>([])
 const ClientesList = ref<{ id: number; name: string }[]>([])
 const ContrapartesList = ref<{ id: number; name: string }[]>([])
 const TribunalesList = ref<{ id: number; name: string }[]>([])
-const TypeModalTittle = ref<Array<string>>(['Tipo de Expediente', 'Abogados', 'Clientes', 'Contrapartes', 'Tribunales'])
+
+const showModalTribunal = ref(false)
+const showModalExpediente = ref(false)
+
 /* @ts-expect-error */
 const URL: string = import.meta.env.VITE_PATH_API
-const TipoExpedienteSelected = ref({ tipexp_tip: '', descri_tip: '' })
+const ExpedienteSelected = ref({ codexp_exp: '', descri_exp: '' })
 const AbogadoSelected = ref({ codabo_abo: '', nombre_abo: '' })
 const ClienteSelected = ref({ codcli_cli: '', nombre_cli: '' })
 const ContraparteSelected = ref({ codcon_con: '', nombre_con: '' })
@@ -94,19 +97,7 @@ function validate(): boolean {
   if (AbogadosList.value.length <= 0)
     return false
 
-  if (ClientesList.value.length <= 0)
-    return false
-
-  if (ContrapartesList.value.length <= 0)
-    return false
-
-  if (TribunalesList.value.length <= 0)
-    return false
-
   if (TipoExpedienteSelected.value === undefined || TipoExpedienteSelected.value.tipexp_tip === '' || TipoExpedienteSelected.value.descri_tip === '')
-    return false
-
-  if (ExpedienteHeader.value.descri_exp === '')
     return false
 
   return true
@@ -528,100 +519,47 @@ onMounted(() => {
   <div class="mt-4">
     <div class="p-6 bg-white rounded-md shadow-md">
       <div>
-        <div class="flex flex-col gap-6 mt-4 sm:grid-cols-2">
+        <!-- aqui estan todos los inputs de audiencia -->
+
+        <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
           <div>
-            <div class="">
-              <label class="text-gray-700" for="motivo_exp">Descripción</label>
-              <input
-                id="motivo_exp" v-model="ExpedienteHeader.descri_exp"
-                type="text"
-                class="w-full h-[5em] mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
-              >
-            </div>
+            <label class="text-gray-700" for="fecini_abo">Fecha de la audiencias</label>
+            <input
+              id="fecini_abo" v-model="AudienciaHeader.fecha_aud"
+              type="date"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+            >
           </div>
-          <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-            <!-- esto son los tipos de expediente -->
-            <div class="">
-              <label class="text-gray-700" for="tipo_espcialidad_abo">Tipo de Expediente</label>
-              <div class="flex gap-2 justify-center items-center">
-                <input
-                  id="fecnac_abo" v-model="TipoExpedienteSelected.tipexp_tip" type="text"
-                  disabled
-                  class="w-[20%] mt-2 border-gray-200  rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
-                >
-
-                <input
-                  id="fecnac_abo" v-model="TipoExpedienteSelected.descri_tip"
-                  type="text"
-                  class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
-                >
-
-                <button
-                  type="button" class="mt-1 p-3  text-sm font-medium text-white bg-sky-700 rounded-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                  @click="presentModal(1)"
-                >
-                  <svg
-                    class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                    />
-                  </svg>
-                  <span class="sr-only">Search</span>
-                </button>
-              </div>
-            </div>
-            <!-- end -->
-
-            <div>
-              <label class="text-gray-700" for="fecini_exp">Fecha de Inicio</label>
-              <input
-                id="fecini_exp" v-model="ExpedienteHeader.fecfin_exp"
-                type="date"
-                class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
-              >
-            </div>
+          <div>
+            <label class="text-gray-700" for="hora_aud">Hora de la audiencia</label>
+            <input
+              id="hora_aud" v-model="AudienciaHeader.hora_aud"
+              type="time"
+              class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+            >
           </div>
-        </div>
 
-        <div class="flex justify-end mt-4">
-          <button
-            type="button" class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            @click="saveExpediente"
-          >
-            Guardar
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <div class="mt-8" />
-    <div class="flex flex-col mt-8 ">
-      <div class="md:flex">
-        <div
-          :class="`p-6 bg-white text-medium text-gray-500 rounded-lg w-full h-[27em] max-h-[27em] ${+(tap === 1) ? '' : 'hidden'}`"
-        >
-          <!-- esto son los tipos de expediente -->
-          <div class="">
-            <label class="text-gray-700" for="tipo_espcialidad_abo">Abogado</label>
+          <div>
+            <label class="text-gray-700" for="tipo-interaccion">Expediente</label>
             <div class="flex gap-2 justify-center items-center">
               <input
-                id="fecnac_abo" v-model="AbogadoSelected.codabo_abo" type="text"
-                disabled
-                class="w-[20%] mt-2 border-gray-200  rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
-              >
-
-              <input
-                id="fecnac_abo" v-model="AbogadoSelected.nombre_abo"
+                id="tipo-interaccion-codigo" v-model="ExpedienteSelected.codexp_exp"
                 type="text"
-                class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
+                disabled
+                class="w-[20%] mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+                readonly
               >
-
+              <input
+                id="tipo-interaccion-nombre" v-model="ExpedienteSelected.descri_expS"
+                type="text"
+                disabled
+                class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+                readonly
+              >
               <button
-                type="button" class="mt-1 p-3  text-sm font-medium text-white bg-sky-700 rounded-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300"
-                @click="presentModal(2)"
+                type="button"
+                class="mt-1 p-3 text-sm font-medium text-white bg-sky-700 rounded-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                @click="showModalExpediente = true"
               >
                 <svg
                   class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -634,67 +572,155 @@ onMounted(() => {
                 </svg>
                 <span class="sr-only">Search</span>
               </button>
+            </div>
+          </div>
 
+          <div>
+            <label class="text-gray-700" for="tipo-interaccion">Tribunal</label>
+            <div class="flex gap-2 justify-center items-center">
+              <input
+                id="tipo-interaccion-codigo" v-model="TribunalSelected.codtri_tri"
+                type="text"
+                disabled
+                class="w-[20%] mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+                readonly
+              >
+              <input
+                id="tipo-interaccion-nombre" v-model="TribunalSelected.descri_tri"
+                type="text"
+                disabled
+                class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500"
+                readonly
+              >
               <button
-                class="flex mt-1 p-3  text-sm font-medium text-white bg-green-500 rounded-lg border hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300"
-                @click="addAbogado(parseInt(AbogadoSelected.codabo_abo), AbogadoSelected.nombre_abo)"
+                type="button"
+                class="mt-1 p-3 text-sm font-medium text-white bg-sky-700 rounded-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                @click="showModalTribunal = true"
               >
                 <svg
-                  xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor" stroke-width="2"
+                  class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 20 20"
                 >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                  <path
+                    stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
                 </svg>
-                <span class="ml-2">Agregar</span>
-              </button>
-
-              <button
-                class="flex mt-1 p-3  text-sm font-medium text-white bg-red-500 rounded-lg border hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300"
-                @click="removeAbogado(AbogadoSelected.codabo_abo)"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor" stroke-width="2"
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-                <span class="ml-2">Eliminar</span>
+                <span class="sr-only">Search</span>
               </button>
             </div>
           </div>
-          <!-- end -->
-          <div class="my-6 flex w-full flex-col">
+
+          <div class="flex justify-end mt-4 col-start-2">
+            <button
+              type="button" class="px-4 py-2 text-gray-200 bg-gray-800 rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+              @click="saveExpediente"
+            >
+              Guardar
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-8" />
+      <div class="flex flex-col mt-8 ">
+        <div class="md:flex">
+          <div
+            :class="`p-6 bg-white text-medium text-gray-500 rounded-lg w-full h-[27em] max-h-[27em] ${+(tap === 1) ? '' : 'hidden'}`"
+          >
+            <!-- esto son los tipos de expediente -->
             <div class="">
-              <div class="rounded-md shadow bg-white h-[17.9em] max-h-[17.9em] overflow-auto">
-                <table class="w-full text-left border-collapse">
-                  <thead class="sticky top-0">
-                    <tr>
-                      <th
-                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+              <label class="text-gray-700" for="tipo_espcialidad_abo">Abogado</label>
+              <div class="flex gap-2 justify-center items-center">
+                <input
+                  id="fecnac_abo" v-model="AbogadoSelected.codabo_abo" type="text"
+                  disabled
+                  class="w-[20%] mt-2 border-gray-200  rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
+                >
+
+                <input
+                  id="fecnac_abo" v-model="AbogadoSelected.nombre_abo"
+                  type="text"
+                  class="w-full mt-2 border-gray-200 rounded-md focus:border-sky-600 focus:ring focus:ring-opacity-40 focus:ring-sky-500" readonly
+                >
+
+                <button
+                  type="button" class="mt-1 p-3  text-sm font-medium text-white bg-sky-700 rounded-lg border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300"
+                  @click="presentModal(2)"
+                >
+                  <svg
+                    class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                  <span class="sr-only">Search</span>
+                </button>
+
+                <button
+                  class="flex mt-1 p-3  text-sm font-medium text-white bg-green-500 rounded-lg border hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300"
+                  @click="addAbogado(parseInt(AbogadoSelected.codabo_abo), AbogadoSelected.nombre_abo)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2"
+                  >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                  </svg>
+                  <span class="ml-2">Agregar</span>
+                </button>
+
+                <button
+                  class="flex mt-1 p-3  text-sm font-medium text-white bg-red-500 rounded-lg border hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300"
+                  @click="removeAbogado(AbogadoSelected.codabo_abo)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor" stroke-width="2"
+                  >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                  <span class="ml-2">Eliminar</span>
+                </button>
+              </div>
+            </div>
+            <!-- end -->
+            <div class="my-6 flex w-full flex-col">
+              <div class="">
+                <div class="rounded-md shadow bg-white h-[17.9em] max-h-[17.9em] overflow-auto">
+                  <table class="w-full text-left border-collapse">
+                    <thead class="sticky top-0">
+                      <tr>
+                        <th
+                          class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                        >
+                          Codigo
+                        </th>
+                        <th
+                          class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
+                        >
+                          nombre
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr
+                        v-for="(value, index) in AbogadosList" :key="index" class="hover:bg-gray-200"
+                        @dblclick="selectAbogado(value)"
                       >
-                        Codigo
-                      </th>
-                      <th
-                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200"
-                      >
-                        nombre
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr
-                      v-for="(value, index) in AbogadosList" :key="index" class="hover:bg-gray-200"
-                      @dblclick="selectAbogado(value)"
-                    >
-                      <td class="px-6 py-4 text-lg text-gray-700 border-b">
-                        {{ value.id }}
-                      </td>
-                      <td class="px-6 py-4 text-gray-500 border-b">
-                        {{ value.name }}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
+                        <td class="px-6 py-4 text-lg text-gray-700 border-b">
+                          {{ value.id }}
+                        </td>
+                        <td class="px-6 py-4 text-gray-500 border-b">
+                          {{ value.name }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
